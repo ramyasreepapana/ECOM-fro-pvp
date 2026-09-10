9a2ab9a71a23a786a0cbf9ea1af365f08c225486
ROHAN-8504 <203j1a4207@raguinstech.com>
2026-09-10 16:07:09 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import {Route,Routes} from 'react-router-dom'
 import { useState } from 'react'
 import './App.css'
 import Header from './Header'
 import Products from './Products'
+import Footer from '../Footer'
+import Home from './Home'
+import Pricing from './Pricing'
+import About from './About'
 function App() {
 
    const [query, setquery] = useState('')
   return (
     <>
       <Header  setquery={setquery}   />
-     <Products  query={query}  />  
+
+     <Routes>
+    <Route path='/'  element={<Home/>} />
+    <Route path='/about'  element={<About/>} />
+    <Route path='/products'  element={<Products  query={query} />} />
+    <Route path='/pricing'  element={<Pricing/>} />
+     </Routes>
+     
+     <Footer/>
      
     </>
   )
